399a9f90a2806683fb43c962d2b3676ee9038950
ncs-randy <randy.wang@ncs.com.sg>
2024-11-03 20:30:15 +0800

    fix

--- utils/modelmapper.js
+++ utils/modelmapper.js
@@ -43,6 +43,38 @@ function mapCreateEventRequestToModel(requestBody) {
     };
 }
 
+function mapEventUpdateBody(requestBody) {
+    const {
+        eventName,
+        eventDescription,
+        eventDate,
+        eventTimeStart,
+        eventTimeEnd,
+        eventLocation,
+        eventType
+    } = requestBody;
+    // Combine eventDate and eventTimeStart to form a DateTime for start
+    const eventStartDateTime = new Date(`${eventDate}T${eventTimeStart}:00`);
+    
+    // Combine eventDate and eventTimeEnd to form a DateTime for end
+    const eventEndDateTime = new Date(`${eventDate}T${eventTimeEnd}:00`);
+
+    const offset = 8 * 60; // GMT+8 in minutes
+    const eventStartDateTimeGMT8 = new Date(eventStartDateTime.getTime() + offset * 60 * 1000);
+    const eventEndDateTimeGMT8 = new Date(eventEndDateTime.getTime() + offset * 60 * 1000);
+
+    return {
+        eventName,
+        eventDescription,
+        eventDate, // DATEONLY format
+        eventTimeStart: eventStartDateTimeGMT8, // Full DateTime
+        eventTimeEnd: eventEndDateTimeGMT8, // Full DateTime
+        eventLocation,
+        eventType
+    }
+}
+
 module.exports = {
-    mapCreateEventRequestToModel
+    mapCreateEventRequestToModel,
+    mapEventUpdateBody
 };
